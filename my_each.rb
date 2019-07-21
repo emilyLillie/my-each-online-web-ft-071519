f2f88a2c4f2f07294195b831adc00a00a73ea075
Emily Lillie <emilylillie3@gmail.com>
2019-07-21 20:08:16 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,17 +2,17 @@
 def my_each(array)
   i = 0 
     while i < array.length 
-<<<<<<< HEAD
+#<<<<<<< HEAD
       yield(array[i])
       i = i + 1 
     end
     array
  end
-=======
+#=======
       yield array[i]
       i = i + 1 
     end
     collection
 end
->>>>>>> e4b381547fb9760099071114c606e04519787da9
+#>>>>>>> e4b381547fb9760099071114c606e04519787da9
 
